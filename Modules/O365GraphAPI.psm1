140d1018bd5b26d813a9713c756b6094478a1c44
Toasterlabs <marc.dekeyser@Hotmail.com>
2018-02-06 12:31:16 -0800

    Initial Push
    Upload module, configuration file and updated readme

---
+++ Modules/O365GraphAPI.psm1
@@ -0,0 +1,174 @@
+﻿Function Get-GraphAPIAuthToken{
+    Param(
+    
+        [parameter(Mandatory=$false)]
+        [STRING]$resource = "https://graph.microsoft.com",
+        [parameter(Mandatory=$true)]
+        [STRING]$ClientID,
+        [parameter(Mandatory=$false)]
+        [STRING]$loginURL= "https://login.microsoftonline.com",
+        [parameter(Mandatory=$true)]
+        [STRING]$tenantdomain,
+        [parameter(Mandatory=$true)]
+        [System.Management.Automation.PSCredential]$credential,
+        [parameter(Mandatory=$false)]
+        [STRING]$redirectUri = "urn:ietf:wg:oauth:2.0:oob"                          
+    )
+
+    # Need the Azure module for this one to work
+    Import-Module Azure
+    
+    # Forming our authority url
+    $authority = "$loginURL/$tenantdomain"
+
+
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+    $AADCredential = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserCredential" -ArgumentList $credential.UserName,$credential.Password
+    
+    # Get the token!
+    $authResult = $authContext.AcquireToken($resource, $clientId,$AADCredential)
+    
+    # Return the token!
+    Return $authResult
+
+} # Token Acquired...
+
+Function Get-GlobalConfig{
+    Param(
+        [parameter(Mandatory=$true)]
+        [STRING]$configFile
+    )
+
+    Write-Output "Loading Global Config File"  
+
+    $config = Get-Content $configFile -Raw | ConvertFrom-Json
+
+    # Returning the configuration for later use
+    return $config;
+
+} # Configuration loaded!
+
+function get-GraphReport{
+    Param(
+        [parameter(Mandatory=$true)]
+        [validateScript({If(Test-Path $_){$true}Else{Throw "Invalid configuration file path: $_"}})]
+        $ConfigFile,
+        
+        [parameter(Mandatory=$true)]
+        [System.Management.Automation.PSCredential]$Credential,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Exchange","Groups","OneDrive","SharePoint","Skype","Tenant","Yammer")]
+        $WorkLoad,  
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("D7","D30","D90","D180")]
+        $Period,
+
+        [Parameter(Mandatory=$false)]
+        $Date
+    )
+
+    DynamicParam {
+            # Set the dynamic parameters' name
+            $ParameterName = 'ReportType'
+            
+            # Create the dictionary 
+            $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+            # Create the collection of attributes
+            $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+            # Create and set the parameters' attributes
+            $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $ParameterAttribute.Mandatory = $true
+            $ParameterAttribute.Position = 1
+            $ParameterAttribute.ParameterSetName = '__AllParameterSets'
+
+            # Add the attributes to the attributes collection
+            $AttributeCollection.Add($ParameterAttribute)
+
+            # Generate and set the ValidateSet
+            If ($Workload -eq "Exchange"){$arrSet = @("EmailActivity","getEmailActivityUserDetail","getEmailActivityCounts","getEmailActivityUserCounts","getEmailAppUsageUserDetail","getEmailAppUsageAppsUserCounts","getEmailAppUsageUserCounts","getEmailAppUsageVersionsUserCounts","getMailboxUsageDetail","getMailboxUsageMailboxCounts","getMailboxUsageQuotaMailboxStatusCounts","getMailboxUsageStorage")}
+            If ($Workload -eq "Groups"){$arrSet = @("getOffice365GroupsActivityDetail","getOffice365GroupsActivityCounts","getOffice365GroupsActivityGroupCounts","getOffice365GroupsActivityStorage","getOffice365GroupsActivityFileCounts")}
+            If ($Workload -eq "OneDrive"){$arrSet = @("getOneDriveActivityUserDetail","getOneDriveActivityUserCounts","getOneDriveActivityFileCounts","getOneDriveUsageAccountDetail","getOneDriveUsageAccountCounts","getOneDriveUsageFileCounts","getOneDriveUsageStorage")}
+            If ($Workload -eq "SharePoint"){$arrSet = @("getSharePointActivityUserDetail","getSharePointActivityFileCounts","getSharePointActivityUserCounts","getSharePointActivityPages","getSharePointSiteUsageDetail","getSharePointSiteUsageFileCounts","getSharePointSiteUsageSiteCounts","getSharePointSiteUsageStorage","getSharePointSiteUsagePages")}
+            If ($Workload -eq "Skype"){$arrSet = @("getSkypeForBusinessActivityUserDetail","getSkypeForBusinessActivityCounts","getSkypeForBusinessActivityUserCounts","getSkypeForBusinessDeviceUsageUserDetail","getSkypeForBusinessDeviceUsageDistributionUserCounts","getSkypeForBusinessDeviceUsageUserCounts","getSkypeForBusinessOrganizerActivityCounts","getSkypeForBusinessOrganizerActivityUserCounts","getSkypeForBusinessOrganizerActivityMinuteCounts","getSkypeForBusinessParticipantActivityCounts","getSkypeForBusinessParticipantActivityUserCounts","getSkypeForBusinessParticipantActivityMinuteCounts","getSkypeForBusinessPeerToPeerActivityCounts","getSkypeForBusinessPeerToPeerActivityUserCounts","getSkypeForBusinessPeerToPeerActivityMinuteCounts")}
+            If ($Workload -eq "Tenant"){$arrSet = @("getOffice365ActivationsUserDetail","getOffice365ActivationCounts","getOffice365ActivationsUserCounts","getOffice365ActiveUserDetail","getOffice365ActiveUserCounts","getOffice365ServicesUserCounts")}
+            If ($Workload -eq "Yammer"){$arrSet = @("getYammerActivityUserDetail","getYammerActivityCounts","getYammerActivityUserCounts","getYammerDeviceUsageUserDetail","getYammerDeviceUsageDistributionUserCounts","getYammerDeviceUsageUserCounts","getYammerGroupsActivityDetail","getYammerGroupsActivityGroupCounts","getYammerGroupsActivityCounts")}
+            
+
+            #$arrSet = Get-ChildItem -Path .\ -Directory | Select-Object -ExpandProperty FullName
+            $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+            # Add the ValidateSet to the attributes collection
+            $AttributeCollection.Add($ValidateSetAttribute)
+
+            # Create and return the dynamic parameter
+            $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+            $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+            return $RuntimeParameterDictionary
+    }
+    
+    # Do this first
+    Begin {
+        # Bind the parameter to a friendly variable
+        $Report = $PsBoundParameters[$ParameterName]
+    }
+
+    # Process is required when using dynamic parameters
+    Process{
+        #If the credential object is empty, prompt the user for credentials
+        if(!$Credential) {$Credential = Get-Credential}
+
+        # Retrieving settings
+        $globalconfig = Get-GlobalConfig -configFile $ConfigFile
+
+        # Setting us up to get the token
+        $ClientID = $globalConfig.AppId
+        $redirectURI = $globalconfig.redirectURI
+        $loginURL = $globalConfig.LoginURL
+        $tenantdomain = $globalConfig.TenantDomain
+        $resource = $globalConfig.ResourceAPI
+
+        # Retrieving our Token
+        $Token = Get-GraphAPIAuthToken -resource $resource -ClientID $ClientID -loginURL $loginURL -tenantdomain $tenantdomain -credential $Credential -redirectUri $redirectURI
+
+        #Build REST API header with authorization token
+        $authHeader = @{
+           'Content-Type'='application\json'
+           'Authorization'=$token.CreateAuthorizationHeader()
+        }
+
+        #If period is specified then add that to the parameters unless it is not supported
+        if($period -and $Report -notlike "*Office365Activation*"){
+            $str = "period='{0}'," -f $Period
+            $parameterset += $str
+        }
+    
+        #If the date is specified then add that to the parameters unless it is not supported
+        if($date -and !($report -eq "MailboxUsage" -or $report -notlike "*Office365Activation*" -or $report -notlike "*getSkypeForBusinessOrganizerActivity*")){
+            $str = "date='{0}'" -f $Date
+            $parameterset += $str
+        }
+        #Trim a trailing comma off the ParameterSet
+        if($parameterset){
+            $parameterset = $parameterset.TrimEnd(",")
+        }
+
+        #Building our URI 
+        $uri = "https://graph.microsoft.com/beta/reports/{0}({1})" -f $report, $parameterset
+
+        # Capturing the result our our query to the graph API in a variable
+        $result = Invoke-RestMethod -Uri $uri –Headers $authHeader –Method Get
+
+        # Since this result is returned in annoying CSV format by default (and the .content is useless), we'll need to convert this...
+        $resultarray = ConvertFrom-Csv -InputObject $result
+    }
+
+    # Do this last
+    End{
+        # Return the results of our query in a proper format we can use for further processing
+        return $resultarray
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # O365GraphAPI
-Powershell functions to retrieve information from the O365 Graph API
+
+## Description
+Powershell functions to retrieve information from the O365 Graph API. Currently able to pull all reports listed on https://developer.microsoft.com/en-us/graph/docs/api-reference/beta/resources/report
+
+Has the following functions:
+
+### get-globalconfig
+*.Param*: ConfigFile
+Imports configuration from json
+
+### Get-Graphoauthtoken
+*.Param*: Resource, ClientID, LoginURL, TenantDomain, RedirectURI, Credentials
+Constructs header for use with other functions. Did not make this work with an app secret, so you need to pass credentials to it. Easiest is to do this with $cred = get-credential
+
+*.Prerequisites*
+Requires the Azure PS module to be installed. Install using "install-module Azure"
+
+### Get-GraphReport
+*Inspired by the script Get-Office365Report from Damien Wiese*
+
+*.Param*: ConfigFile, Workload, ReportType, Period, Date
+Reads information from the configfile, gets the token, retrieves the report, and converts it to a useable format for further processing
+
+*.Usage*
+get-GraphReport -configfile <Path to config JSON> -Workload <Workload to report from> -ReportType <Type of report to get based on workload> -Credential <Credential to retrieve token with> -period <Period to run report against> 
+get-GraphReport -configfile <Path to config JSON> -Workload <Workload to report from> -ReportType <Type of report to get based on workload> -Credential <Credential to retrieve token with> -date <Specific day to run report against>
+
+## Configuration
+Requires a graph API application to be setup in the azure management portal . Set the redirect URI to "urn:ietf:wg:oauth:2.0:oob". Not really using this, but it needs to be set...
+
+Configuration file layout:
+
+```
+{
+    "Description": "Global configuration file for O365 Investigations",
+    "AppID": "<App ID goes here>",
+    "TenantDomain": "<your tenant domain (contoso.onmicrosoft.com or contoso.com)>",
+    "LoginURL" : "https://login.windows.net",
+    "ResourceAPI" : "https://manage.office.com"
+	"redirectURI" : "urn:ietf:wg:oauth:2.0:oob" 
+}
+```
+
+## Usage
+Install azure PS module:
+install-module Azure
+
+Import module:
+Import-module <path to O365graphap.psm1> -force
+
+Run report:
+get-graphreport -configfile <Path to config JSON> -Workload <Workload to report from> -ReportType <Type of report to get based on workload> -Credential <Credential to retrieve token with> -date <Specific day to run report against>
+
+## Disclaimer
+Use this code at your own risk, verify anything you run in production. If you decide to run this code all risk stays with you. 
+
+### Legalese:
+> The sample scripts are provided AS IS without warranty of any kind. I disclaim all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event shall I,or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if I have been advised of the possibility of such damages.
